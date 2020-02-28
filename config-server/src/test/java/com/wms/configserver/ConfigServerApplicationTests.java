47d940ee94b479634bc95c95527a57298ca531d6
王明世 <wangmingshi@qtopay.cn>
2020-02-28 17:16:18 +0800

    springCloud 配置中心

---
+++ config-server/src/test/java/com/wms/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.wms.configserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
